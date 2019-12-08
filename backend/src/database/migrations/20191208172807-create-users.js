7c0cf1bcb1c465dcefa07aca9d76193dcc4fcbce
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-12-08 14:46:29 -0300

    configs db e migrations

---
+++ backend/src/database/migrations/20191208172807-create-users.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users',{
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        autoIncrement: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        autoIncrement: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
