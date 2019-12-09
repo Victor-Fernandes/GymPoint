745b394e95c527cdc8cf1f2777dbcca3fdc103ba
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-12-09 17:23:11 -0300

    criando usercontroller e router de create session

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '20e14f4c1d968b1e6b2127db86c04b96',
+  expiresIn: '7d',
+};
--- backend/src/database/migrations/20191208172807-create-users.js
+++ backend/src/database/migrations/20191208172807-create-users.js
@@ -1,40 +1,37 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users',{
+    return queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
-        primaryKey: true
+        primaryKey: true,
       },
       name: {
         type: Sequelize.STRING,
         allowNull: false,
-        autoIncrement: true
       },
       email: {
         type: Sequelize.STRING,
         allowNull: false,
-        autoIncrement: true
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       created_at: {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
       },
       updated_at: {
         type: Sequelize.DATE,
-        allowNull: false
-      }
+        allowNull: false,
+      },
     });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
 };
