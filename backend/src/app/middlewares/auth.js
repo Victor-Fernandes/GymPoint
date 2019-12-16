0d3d14fd908452b84d8cce9a7adda795ea548dc3
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-12-16 17:28:37 -0300

    corrigindo validação do update user

--- backend/src/app/middlewares/auth.js
+++ backend/src/app/middlewares/auth.js
@@ -7,7 +7,7 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ error: 'Token not found' }  );
+    return res.status(401).json({ error: 'Token not found' });
   }
 
   const [, token] = authHeader.split(' ');
@@ -16,8 +16,6 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
-
-    return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
   }
