670d049dc0d5ebb9671d3cd62e63e51ad8df63bb
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-12-18 08:10:32 -0300

    create students migrations

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -31,7 +31,7 @@ class UserController {
         .json({ error: 'password and confirm password are not the same' });
     }
 
-    const { name, email, password } = await User.create(req.body);
+    const { name, email } = await User.create(req.body);
 
     return res.json({
       name,
@@ -43,22 +43,22 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
-      oldPassword: Yup.string().min(6),
+      oldPassword: Yup.string(),
       password: Yup.string()
         .min(6)
-        .when('oldPassword', (oldPassword, field) => {
-          oldPassword ? field.required() : field;
-        }),
-      confirmPassword: Yup.string().when('password', (password, field) => {
-        password ? field.required().oneOf([Yup.ref('password')]) : field;
-      }),
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(401).json({ error: 'validation fail' });
+      return res.status(400).json({ error: 'validations fails' });
     }
 
-    const { name, email, password, oldPassword, confirmPassword } = req.body;
+    const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
 
@@ -74,12 +74,11 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match.' });
     }
 
+    const { name } = await user.update(req.body);
+
     return res.json({
       name,
       email,
-      password,
-      oldPassword,
-      confirmPassword,
     });
   }
 }
